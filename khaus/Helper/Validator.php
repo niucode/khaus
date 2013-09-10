dd7bdc8e0d6b5c34ef9a70f3bef85e2c673aaf7f
rhyudek1 <rhyudek1@gmail.com>
2013-09-10 17:18:37 -0300

    update form validator

--- khaus/Helper/Validator.php
+++ khaus/Helper/Validator.php
@@ -52,42 +52,4 @@ class Khaus_Helper_Validator
     {
         return preg_match('/^[0-9]+$/', $numeric);
     }
-    
-    public static function char($string, $min, $max, $caseInsensitive = true, $error = '')
-    {
-        $ci = $caseInsensitive ? 'i' : '';
-        $pattern = sprintf('/^[a-z]{%d,%d}$/%s', $min, $max, $ci);
-        if (!preg_match($pattern, $string)) {
-            if (empty($error)) {
-                $errorMessage = "Solo se permiten de $min a $max caracteres";
-            }
-            throw new Khaus_Helper_Exception($errorMessage);
-        }
-        return true;
-    }
-    
-    public static function digit($digits, $min, $max, $error = '')
-    {
-        $pattern = sprintf('/^[0-9]{%d,%d}$/', $min, $max);
-        if (!preg_match($pattern, $string)) {
-            if (empty($error)) {
-                $errorMessage = "Solo se permiten numeros hasta $max caracteres de longitud";
-            }
-            throw new Khaus_Helper_Exception($errorMessage);
-        }
-        return true;
-    }
-    
-    public static function alnum($string, $min, $max, $caseInsensitive = true, $error = '')
-    {
-        $ci = $caseInsensitive ? 'i' : '';
-        $pattern = sprintf('/^[a-z0-9]{%d,%d}$/%s', $min, $max, $ci);
-        if (!preg_match($pattern, $string)) {
-            if (empty($error)) {
-                $errorMessage = "Solo se permiten de $min a $max caracteres alfanumericos";
-            }
-            throw new Khaus_Helper_Exception($errorMessage);
-        }
-        return true;
-    }
 }
